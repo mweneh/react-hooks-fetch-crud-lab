7b743949e5afc8bb08594adc4a1d5044b9a363dc
David <davidongaroh@gmail.com>
2022-07-20 21:13:49 +0300

    Done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+
+function QuestionItem({ question, onDelete, onAnsChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,16 +9,22 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
-
+  function handleDelete(){
+    //delete quiz from server &list
+    onDelete(question)
+  }
+function handleChange(e){
+  onAnsChange(question, e.target.value)
+}
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React ,{useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+   const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(r => r.json())
+    .then(quiz => setQuestions(quiz))
+  },[])
+
+function handleUpdateOnDelete(deletedQuiz) {
+  const serverOptions = {method: 'DELETE'}
+  fetch(`http://localhost:4000/questions/${deletedQuiz.id}`,serverOptions)
+  .then(r => r.json())
+  .then(() => setQuestions(questions => questions.filter(quiz => quiz.id!== deletedQuiz.id)))
+}
+
+   const qList = questions.map(q => (
+    <QuestionItem  
+    onDelete={handleUpdateOnDelete} 
+    question={q} 
+    key={q.key} 
+    onAnsChange = {handleAnsChange}/>
+  ))
+
+  function handleAnsChange(id, correctIndex) {
+    const options = {
+      method:'PATCH',
+      headers: {
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify({correctIndex})
+    }
+    fetch(`http://localhost:4000/questions/${id}`, options)
+    .then (r => r.json)
+    .then((updatedQuiz) =>{
+      const updatedQuestions= questions.map(q =>{
+        if (q.id ===updatedQuiz.id) return updatedQuiz
+        return q
+      })
+      setQuestions(updatedQuestions)
+    })
+
+
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{qList}</ul>
     </section>
   );
 }
